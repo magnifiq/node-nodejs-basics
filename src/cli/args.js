39410a86f7b7b9457274f48f1979e1f4d207a0f2
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-01-28 18:55:48 +0100

    Finish cli part

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arrArgs = process.argv.slice(2)
+    const res = {}
+    while (arrArgs.length > 0) {
+        const argName = arrArgs.shift().slice(2);
+        const argValue = arrArgs.shift();
+
+        res[argName]=argValue
+    }
+
+    const end=Object.entries(res).map(([name,val])=>`${name} is ${val}`).join(", ")
+    console.log(end)
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    //console.log(process.env)
+    const envVars = Object.keys(process.env).filter(el => el.startsWith("RSS_"));
+    //console.log(envVars)
+    const res=envVars.map(el=>`${el}=${process.env[el]}`) 
+    console.log(res.join("; "))
 };
 
 parseEnv();
