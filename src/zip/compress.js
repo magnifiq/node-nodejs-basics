f8d27e68cbbf7a56d80f419fa76b53b7a5a718c1
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-01-28 21:02:19 +0100

    Finish it

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import {createReadStream, createWriteStream} from "fs"
+import { pipeline } from "stream";
+import { createGzip } from "zlib";
+
+const srcFile = createReadStream("./src/zip/files/fileToCompress.txt")
+const zipStr=createGzip()
+const zipFile = createWriteStream("./src/zip/files/archive.gz")
 const compress = async () => {
-    // Write your code here 
+    pipeline(
+        srcFile,
+        zipStr,
+        zipFile,
+        err=>console.error(err)
+    )
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from "fs"
+import { pipeline } from "stream";
+import { createGunzip } from "zlib";
+
+
+const zipFile = createReadStream("./src/zip/files/archive.gz")
+const unzipStr=createGunzip()
+const unzipFile = createWriteStream("./src/zip/files/fileToCompress.txt") 
 const decompress = async () => {
-    // Write your code here 
+    pipeline(
+        zipFile,
+        unzipStr,
+        unzipFile,
+        err=>console.error(err)
+    )
 };
 
 await decompress();
