587cc9529e4d39b65799da143dee1614c7590124
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-01-28 21:43:04 +0100

    Update read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,5 @@
 import { createReadStream } from "fs";
-
+console.log("start")
 const readStr = createReadStream("./src/streams/files/fileToRead.txt");
 const read = async () => {
     readStr.pipe(process.stdout)
