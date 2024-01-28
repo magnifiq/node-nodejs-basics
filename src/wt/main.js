710f93ef4bddb24c291ef262e5a868a9deee9516
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-01-28 21:27:37 +0100

    finish wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import {cpus} from "os"
+import { resolve } from "path"
+import {Worker} from "worker_threads"
+
+const BEGIN=10
+
+const workFib=(num)=>{
+    return new Promise(resolve=>{
+        const worker = new Worker("./src/wt/worker.js", {workerData:num})
+
+        worker.on('message', data=>resolve({
+            status:"resolved",
+            data
+        }))
+
+        worker.on('error', () => resolve({
+            status: "error",
+            data:null
+        }))
+    })
+}
 const performCalculations = async () => {
-    // Write your code here
+    const workersPool=Array.from({length:cpus().length}, (_,num)=>workFib(BEGIN+num))
+    const res=await Promise.all(workersPool)
+    console.log(res)
 };
 
 await performCalculations();
