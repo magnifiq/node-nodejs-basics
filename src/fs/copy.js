5d0a4064e23973498998c69ae0bed17491831a67
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-03-12 22:33:00 +0100

    Update copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 
 const copy = async () => {
     try {
-        
+        await fs.cp("./src/fs/files", "./src/fs/files_copy", { recursive: true});
     } catch {
         throw new Error("FS operation failed");
     }
